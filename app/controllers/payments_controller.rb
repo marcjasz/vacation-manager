2a6b538984233cdc5d129840d915a2ac1a9f915b
Marcin Jachymski <marcin.jachymski@bitgamelabs.com>
2020-02-06 00:02:21 +0100

    add payment form to user view

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -69,6 +69,6 @@ class PaymentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def payment_params
-      params.require(:payment).permit(:amount, :date)
+      params.permit(:amount, :date, :participation_id)
     end
 end
