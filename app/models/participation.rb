e191fcf407841f30ea67c5524c6c18131e40c4bd
Marcin Jachymski <marcin.jachymski@bitgamelabs.com>
2020-02-08 18:44:40 +0100

    fix payment modals

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,4 +2,15 @@ class Participation < ApplicationRecord
     belongs_to :participant
     belongs_to :group
     has_many :payments, inverse_of: :participation
+    delegate :vacation_id, to: :group
+
+    validate :participant_unique_per_vacation
+
+    private
+
+    def participant_unique_per_vacation
+        if participant.groups.map { |group| group.vacation_id }.uniq.include?(vacation_id: id)
+            errors.add :participation, 'participant is already assigned to this vacation'
+        end
+    end
 end
