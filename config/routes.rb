b42b9ad1b7f7596b39e3d3555e7ada78b41c85d2
Marcin Jachymski <marcin.jachymski@bitgamelabs.com>
2020-02-12 19:59:36 +0100

    adjust route params

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
   root 'vacations#index'
-  resources :lodgings
-  post '/lodgings/:id/assign_group', to: 'lodgings#assign_group', as: 'add_lodging_to_group'
-  post '/lodgings/:id/remove_group/:group_id', to: 'lodgings#remove_group', as: 'remove_lodging_from_group'
-  resources :service_providers
-  resources :invoices
-  resources :organizers
-  resources :vacations
-  resources :counselors
-  post '/counselors/:id/assign_group', to: 'counselors#assign_group', as: 'add_counselor_to_group'
-  post '/counselors/:id/remove_group/:group_id', to: 'counselors#remove_group', as: 'remove_counselor_from_group'
+  resources :lodgings, param: :name
+  post '/lodgings/:name/assign_group', to: 'lodgings#assign_group', as: 'add_lodging_to_group'
+  post '/lodgings/:name/remove_group/:group_id', to: 'lodgings#remove_group', as: 'remove_lodging_from_group'
+  resources :service_providers, param: :nip
+  resources :invoices, param: :invoice_number
+  resources :organizers, param: :nip
+  resources :vacations, param: :name
+  resources :counselors, param: :pesel
+  post '/counselors/:pesel/assign_group', to: 'counselors#assign_group', as: 'add_counselor_to_group'
+  post '/counselors/:pesel/remove_group/:group_id', to: 'counselors#remove_group', as: 'remove_counselor_from_group'
   resources :payments
-  resources :participations
   resources :groups
-  resources :participants
-  post '/participants/:id/assign_group', to: 'participants#assign_group', as: 'add_participant_to_group'
-  post '/participants/:id/remove_group/:group_id', to: 'participants#remove_group', as: 'remove_participant_from_group'
+  resources :participants, param: :pesel
+  post '/participants/:pesel/assign_group', to: 'participants#assign_group', as: 'add_participant_to_group'
+  post '/participants/:pesel/remove_group/:group_id', to: 'participants#remove_group', as: 'remove_participant_from_group'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
